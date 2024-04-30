436c176ea098939b645ffd4adb54a880e3bd41f8
Subash <subashmurugan2021@gmail.com>
2024-04-30 14:59:00 +0530

    Create and Configure Firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const config = {
+  apiKey: "AIzaSyAZcnw7SpN0WmD6Ux6IE4-y0-n3pEWKqco",
+  authDomain: "chat-app-1acec.firebaseapp.com",
+  projectId: "chat-app-1acec",
+  storageBucket: "chat-app-1acec.appspot.com",
+  messagingSenderId: "167867927150",
+  appId: "1:167867927150:web:84bc699e630e103557f47f"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(config);
+
+
+
